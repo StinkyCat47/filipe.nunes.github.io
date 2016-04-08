d0476826f3047cb74ffdbc5800acf3ea5380d0b5
filipe-nunes <filnunes@gmail.com>
2016-04-08 16:37:52 +0000

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -6,12 +6,21 @@ $( document ).ready(function() {
   
   var feet = 0;
   
+  
+  //
+  // Maybe split by ('') would be best, which was replaced in commit a9fc97dd787f8d2646dd13065c2201e420e25305
+  //
   var content = $('#lol').text().split('x');
   
   for(var i = 0; i < content.length;i = i + 3) {
     if($.isNumeric(content[i])) {
       firstNumStr += content[i];
       secondNumStr += content[i+1];
+      
+      //
+      // TODO!
+      //
+      
       var thirdNumStrAux = content[i+2].split('');
       for(var j = 0; j < thirdNumStrAux.length; j++) {
         if($.isNumeric(thirdNumStrAux[j])) {
