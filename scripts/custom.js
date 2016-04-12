8a62b9dc3e5ae014b3c945b1026db36ce2c6bee3
filipe-nunes <filnunes@gmail.com>
2016-04-12 21:35:15 +0000

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,12 +1,39 @@
 $( document ).ready(function() {
   
-  var steps = $.trim($('#lol').text());
+  function addHouseIfNew() {
+    for(var i = 0; i < houses.length; ++i) {
+      if(houses[i].x == x && houses[i].y == y) {
+        return;
+      }
+      
+      houses.push({x = x, y = y});
+    }
+  }
   
+  var steps = $.trim($('#lol').text());
+  var x = 0;
+  var y = 0;
+  var houses = [{x = x, y = y}];
   for(var i = 0; i < steps.length;++i) {
     var move = steps[i];
+    
+    if(move == '>') {
+      // RIGHT
+      ++x;
+    } else if(move == '<') {
+      // LEFT
+      --x;
+    } else if(move == '^') {
+      // UP
+      ++y;
+    } else {
+      // DOWN
+      --y;
+    }
+    addHouseIfNew();
   }
 
-  $('#result').text('Result = ' + feet);
+  $('#result').text('Result = ' + houses.length);
 });
 
 function sortNumber(a,b) {
