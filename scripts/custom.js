3193362621b31d42ecee1f4bf5c044823f461f33
filipe-nunes <filnunes@gmail.com>
2016-04-18 09:31:21 +0000

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -5,6 +5,13 @@ function doIt() {
   var ruleThree = ['ab', 'cd', 'pq', 'xy'];
   
   var input = $('#lol').text();
+  var inputArr = input.split('\n');
+  
+  for(var i = 0; i < inputArr.length; ++i) {
+    //
+    // DO TESTS!
+    //
+  }
   
   $('#result').text('Result = ' + result);
 }
